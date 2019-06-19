3bdcc3fa0feb33e6d1898c9d35e87cfd072ae14e
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2019-06-19 23:20:52 +1000

    Created a second class called Lighting which could be used to instantiate a series of lights for a show.

--- Python_deep_dive_part_1/classes.py
+++ Python_deep_dive_part_1/classes.py
@@ -16,3 +16,23 @@ full_cream_latte.brew()
 # Create the second object from the coffee class
 skim_flat_white = coffee('flat white', 'skim')
 skim_flat_white.brew()
+
+
+class Lighting:
+
+    def __init__(self, name: str, colour: str):
+        Lighting.name = name
+        Lighting.colour = colour
+
+    def light_on(self):
+        print('---------------------------------')
+        print('Lighting turned on')
+
+    def light_off(self):
+        print('---------------------------------')
+        print('Light turning off')
+
+
+dmx_1 = Lighting('dmx_1', 'white')
+dmx_1.light_on()
+dmx_1.light_off()
